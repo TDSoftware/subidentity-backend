6378df8a7dfd21262e7ba2c5d81bdf375b21af37
Vani Narayana <v.narayana@tdsoftware.de>
2022-05-17 13:59:14 +0200

    unit test cases for chain router

---
+++ src/routes/chains/chainRouter.test.ts
@@ -0,0 +1,25 @@
+import { request, setupTests } from "../../lib/testSetup";
+
+setupTests();
+
+jest.mock("@npmjs_tdsoftware/subidentity");
+
+const wsProvider = "ws://fake.io";
+
+describe("GET /chains/status", () => {
+    it("should fetch the chain status", async () => {
+        const response = await request().get("/chains/status?wsProvider="+wsProvider);
+        expect(response.statusCode).toBe(200);
+        expect(response.body.chainStatus.isIndexed).toBe(false);
+        expect(response.body.chainStatus.isArchiveNode).toBe(true);
+        expect(response.body.chainStatus.implementsIdentityPallet).toBe(true);
+        expect(response.body.chainStatus.chainName).toBe("fake-chain-name");
+    });
+
+    it("should throw error since query parameter is missing", async () => {
+        const response = await request().get("/chains/status");
+        expect(response.statusCode).toBe(400);
+        expect(response.body.error).toBe("Query parameter missing:wsProvider");
+    });
+
+});
