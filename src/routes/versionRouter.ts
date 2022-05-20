d87e3f8c0a4fa711e67c8e753611c3d8b031dc5d
Vani Narayana <v.narayana@tdsoftware.de>
2022-05-20 11:08:46 +0200

    added commitHash to version route

--- src/routes/versionRouter.ts
+++ src/routes/versionRouter.ts
@@ -1,10 +1,14 @@
+import { execSync } from "child_process";
 import { Router, Request, Response } from "express";
 import packageJSON from "../../package.json";
 
 export const versionRouter = Router();
 
+const gitCommand = "git rev-parse HEAD";
+
 versionRouter.get("/", async (req: Request, res: Response) => {
     res.send({
-        version: packageJSON.version
+        version: packageJSON.version,
+        commitHash: execSync(gitCommand).toString().slice(0, 7)
     });
 });
