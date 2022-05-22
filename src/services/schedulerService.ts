2772d1e55b88082f8bdd1fb14d5fd6d0f4f744b6
a-oertel <a.oertel@tdsoftware.de>
2022-05-22 23:52:33 +0300

    use unpaged get all Identities

--- src/services/schedulerService.ts
+++ src/services/schedulerService.ts
@@ -2,23 +2,21 @@
 import { chainRepository } from "../repositories/chainRepository";
 import { identityRepository } from "../repositories/identityRepository";
 import { accountRepository } from "../repositories/accountRepository";
-import { getIdentities } from "@npmjs_tdsoftware/subidentity";
+import { getCompleteIdentities } from "@npmjs_tdsoftware/subidentity";
 import { ChainsWsProvider } from "../types/dtos/ChainsWsProvider";
 
 
 export const schedulerService = {
     async fetchIdentities(): Promise<void>{
-        console.log("fetchIdentities");
         const chains: ChainsWsProvider[] = await chainRepository.getAllWithFirstWsProvider();
         let identities;
-        console.log(chains);
         chains.forEach( async (chain: ChainsWsProvider) => {
-            identities = await getIdentities(chain.ws_provider,1,9999);
+            identities = await getCompleteIdentities(chain.ws_provider);
             await accountRepository.insertOrUpdateAccountsOfIdentities(
-                identities.items, chain.id
+                identities, chain.id
             );
             identityRepository.insertOrUpdateAll(
-                identities.items, chain.id
+                identities, chain.id
             );
             //TODO: get and save judgements
         });
